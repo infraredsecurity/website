dd3c1c0d687268c34b38005d1bb962e771b1177e
Eric Sheridan <eric@infraredsecurity.com>
2023-08-29 15:23:26 -0400

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,15 @@ $(document).ready(function() {
     }
 
     // display 'users' and 'reports' menus if user has permission to create users
-    fetch('/lms/blocks/iomad_company_admin/company_user_create_form.php', { method: 'HEAD' })
+    let headers = {
+        'pragma': 'no-cache',
+        'cache-control': 'no-cache',
+    };
+    let request = {
+        method: 'HEAD',
+        headers: headers
+    };
+    fetch('/lms/blocks/iomad_company_admin/company_user_create_form.php', request)
         .then((response) => {
             if (response.status == 200) {
                 console.log('enabling company admin menus');
