186fd60f76d9e5df9b9c4b6a588c24cb761cd76b
Eric Sheridan <eric@infraredsecurity.com>
2023-09-07 21:39:06 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    console.log('admin menu callback triggered');
+    console.log('hack callback triggered');
     
     // display admin menus
     if ($('#quicklinks li.item-admin').length > 0) {
@@ -13,6 +13,15 @@ $(document).ready(function() {
         $('.mitem_users').attr('style', 'display: flex !important');
         $('.mitem_reports').attr('style', 'display: flex !important');
     }
+
+    // make 'Enroll now' do something meaningful if user is guest
+    console.log('updating buttons');
+    $('#page-enrol-index .enrol-sidebar .course-enrolbtn').on('click', function(e) {
+        if ($('#master-header .header-tools .tool-login').length > 0) {
+            e.preventDefault();
+            $('#page-enrol-index #region-main #page-content form button.btn-primary').click();
+        }
+    });
 
     // convert uk-english to us-english
     console.log('converting to us-english...');
@@ -35,5 +44,5 @@ $(document).ready(function() {
     $('#page-enrol-index #region-main #page-content .collapsible span.sr-only').each(englishCallback);
     $('#page-enrol-index #region-main #page-content .collapsible h3').each(englishCallback);
 
-    console.log('admin menu callback finished');
+    console.log('hack callback finished');
 });
